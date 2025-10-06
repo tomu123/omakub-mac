56c5efb855c8046c6b38c5ca58b667d985e3ea9d
Tomu Komatsu <tomu5642514@gmail.com>
2025-10-06 16:54:49 -0500

    fix libraries

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -2,6 +2,6 @@
 
 brew install \
   pkgconf autoconf bison \
-  openssl zlib readline libyaml ncurses libffi gdbm jemalloc \
+  openssl@3 openssl@1.1 zlib readline libyaml ncurses libffi gdbm jemalloc gmp\
   vips imagemagick mupdf mupdf-tools \
-  redis sqlite mysql-client postgresql
+  redis sqlite mysql-client libpq
