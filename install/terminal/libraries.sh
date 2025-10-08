c72a8136d4271d6854f1c78c454a9bca2cb262f4
Tomu Komatsu <tomu5642514@gmail.com>
2025-10-07 19:22:40 -0500

    remove mupdf-tools in libraries

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -3,5 +3,5 @@
 brew install \
   pkgconf autoconf bison \
   openssl@3 rbenv/tap/openssl@1.1 zlib readline libyaml ncurses libffi gdbm jemalloc gmp\
-  vips imagemagick mupdf mupdf-tools \
+  vips imagemagick mupdf \
   redis sqlite mysql-client libpq
