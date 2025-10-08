3df88b1c575201c019da7a0a418c08d7041dc8d4
Tomu Komatsu <tomu5642514@gmail.com>
2025-10-07 19:13:07 -0500

    fix openssl@1.1

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -2,6 +2,6 @@
 
 brew install \
   pkgconf autoconf bison \
-  openssl@3 openssl@1.1 zlib readline libyaml ncurses libffi gdbm jemalloc gmp\
+  openssl@3 rbenv/tap/openssl@1.1 zlib readline libyaml ncurses libffi gdbm jemalloc gmp\
   vips imagemagick mupdf mupdf-tools \
   redis sqlite mysql-client libpq
