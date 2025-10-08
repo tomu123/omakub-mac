af3cabe052ae45a1094e623a5a638e99f8426373
Tomu Komatsu <tomu5642514@gmail.com>
2025-10-07 23:07:21 -0500

    fix zellij configuration and always use omakub-mac configurations

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -3,9 +3,6 @@
 # Display system information in the terminal
 brew install fastfetch
 
-# Only attempt to set configuration if fastfetch is not already set
-if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
-  # Use Omakub-mac fastfetch config
-  mkdir -p ~/.config/fastfetch
-  cp ~/.local/share/omakub-mac/configs/fastfetch.jsonc ~/.config/fastfetch/config.jsonc
-fi
+# Use Omakub-mac fastfetch config
+mkdir -p ~/.config/fastfetch
+cp ~/.local/share/omakub-mac/configs/fastfetch.jsonc ~/.config/fastfetch/config.jsonc
--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -3,5 +3,5 @@
 brew install zellij
 
 mkdir -p ~/.config/zellij/themes
-[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp ~/.local/share/omakub-mac/configs/zellij.kdl ~/.config/zellij/config.kdl
+cp ~/.local/share/omakub-mac/configs/zellij.kdl ~/.config/zellij/config.kdl
 cp ~/.local/share/omakub-mac/themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
